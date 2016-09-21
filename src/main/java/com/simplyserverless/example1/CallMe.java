fabcfb32e81e407aac24b5c666ebcb6788583b52
danhanley <hanley.dan@gmail.com>
2016-09-21 21:09:03 +0100

    Mapping Request Object

--- src/main/java/com/simplyserverless/example1/CallMe.java
+++ src/main/java/com/simplyserverless/example1/CallMe.java
@@ -1,4 +1,5 @@
 package com.simplyserverless.example1;
+import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -7,8 +8,13 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 public class CallMe implements RequestHandler<Request,Response>{
     public  Response handleRequest(Request request, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("Hello Logs");
-
-        return new Response("The server says: " + request.param1 + " was recieved and returned.");
+        logger.log("Hello Logs!!!");
+        if (request.getParams() != null)
+            if (request.getParams().get("querystring") != null)
+                return new Response("Query parameter q1 = " + request.getParams().get("querystring").get("q1"));
+            else
+                return new Response("No query params found");
+        else
+            return new Response("No params found");
     }
 }
--- src/main/java/com/simplyserverless/example1/Request.java
+++ src/main/java/com/simplyserverless/example1/Request.java
@@ -1,24 +1,54 @@
 package com.simplyserverless.example1;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by dan on 13/09/16.
+ *
+ * ##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+ body-json={}, params={path={}, querystring={q1=Fred, q2=Perry}, header={}}, stage-variables={}, context={account-id=534737636834, api-id=qvpphez67b, api-key=test-invoke-api-key, authorizer-principal-id=, caller=534737636834, cognito-authentication-provider=, cognito-authentication-type=, cognito-identity-id=, cognito-identity-pool-id=, http-method=GET, stage=test-invoke-stage, source-ip=test-invoke-source-ip, user=534737636834, user-agent=Apache-HttpClient/4.5.x (Java/1.8.0_102), user-arn=arn:aws:iam::534737636834:root, request-id=test-invoke-request, resource-id=qnutaqa1o0, resource-path=/}
+
  */
 public class Request {
-    public String getParam1() {
-        return param1;
+
+
+
+    public Map<String, String> bodyJSON;
+    public Map<String, Map<String,String>> params;
+    public Map<String, String> stageVariables;
+    public Map<String, String> context;
+
+    public Map<String, String> getBodyJSON() {
+        return bodyJSON;
     }
 
-    public void setParam1(String param1) {
-        this.param1 = param1;
+    public void setBodyJSON(Map<String, String> bodyJSON) {
+        this.bodyJSON = bodyJSON;
     }
 
-    String param1;
-
-
-    public Request(String param1) {
-        this.param1 = param1;
+    public Map<String, Map<String, String>> getParams() {
+        return params;
     }
 
-    public Request() {
+    public void setParams(Map<String, Map<String, String>> params) {
+        this.params = params;
     }
+
+    public Map<String, String> getStageVariables() {
+        return stageVariables;
+    }
+
+    public void setStageVariables(Map<String, String> stageVariables) {
+        this.stageVariables = stageVariables;
+    }
+
+    public Map<String, String> getContext() {
+        return context;
+    }
+
+    public void setContext(Map<String, String> context) {
+        this.context = context;
+    }
+
 }
